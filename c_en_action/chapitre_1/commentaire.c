093bba389194b75ffe3f37ea547bc7de079b8f2f
ehrhart <ehrhart@imac-de-ehrhart-2.home>
2016-08-02 02:27:56 +0200

    first work on new book c_in_action chapitre 1

---
+++ c_en_action/chapitre_1/commentaire.c
@@ -0,0 +1,24 @@
+/* ceci est un commentaire */
+
+// ceci est un commentaire
+
+/*
+ceci
+est un
+commentaire */
+
+/* ceci
+ * est un 
+ * commentaire
+ */
+
+/* Pour ne pas compilier une section du code il est possible de
+ * l'encadrer par un DEFINE jamais defini
+ * EX:
+ * #ifdef NEPASCOMPILIER
+ * a = 3;
+ * if (a == 3)
+ * 	printf("Bravo\n");
+ * #endif
+ */
+
